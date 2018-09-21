ebe0e37ca629e5d3723289d9c38f390059e10936
samim <samimfazlu@gmail.com>
2018-09-21 15:57:29 +0600

    Complete let const concept

---
+++ main.js
@@ -0,0 +1,36 @@
+//Problem
+// var firstName = "samim";
+// //1000 line code
+// var firstName = "Anis"; //Intentional
+
+//scoping Issue
+// const age = 26;
+// if (age > 18) {
+//   const canVote = "I can vote"; //tmp variable
+//   console.log(canVote);
+// }
+
+//console.log(canVote);
+
+// {
+//   let name = "samim";
+// }
+// console.log(name);
+
+// function prinbtNumber() {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+
+//   console.log(i, "Not needed");
+// }
+// prinbtNumber();
+
+//Solution
+
+// let firstName = "samim";
+
+//  firstName = "Anis";
+// const firstName = "samim";
+// firstName = "Anis";
+// console.log(firstName);
