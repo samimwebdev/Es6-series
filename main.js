1b914b2c9a11900d748967f70f75415254aad8d2
samim <samimfazlu@gmail.com>
2018-09-21 19:37:22 +0600

    Arrow function

--- main.js
+++ main.js
@@ -6,8 +6,28 @@
 
 //Function expression
 
-const add = function(num1 = 2, num2) {
-  return num1 + num2;
+//Arrow function
+// const add = (num1 = 2, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(add(undefined, 1));
+
+//If you return a single statement don't have to use {}
+//const add = (num1 = 2, num2) => num1 + num2;
+
+//if you want to use single paramete don't have to use ()
+//const add = num1 => num1 + 3;
+//console.log(add(1));
+
+//Where not to use arrow function
+const self = {
+  firstName: "samim",
+  age: 25,
+  showBio: function() {
+    console.log(this);
+    return `I am ${this.firstName} and I am ${this.age} `;
+  }
 };
 
-console.log(add(undefined, 1));
+console.log(self.showBio());
