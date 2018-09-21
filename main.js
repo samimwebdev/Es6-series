fe7d348043df13b3c9f29727b9a51e7610ef8a98
samim <samimfazlu@gmail.com>
2018-09-21 19:01:53 +0600

    Deault function parameter

--- main.js
+++ main.js
@@ -1,41 +1,13 @@
-const firstName = "samim";
-const lastName = "Fazlu";
-const age = 25;
-const profession = "web developer";
-// function showBio() {
-//   return (
-//     "my name is " +
-//     firstName +
-//     " " +
-//     lastName +
-//     " and I am " +
-//     age +
-//     " I am a " +
-//     profession
-//   );
+//Number addition
+//Function statement
+// function add(num1 = 3, num2) {
+//   return num1 + num2;
 // }
 
-// function showBio() {
-//   //Template string
-//   return `My name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} and I am ${age -
-//     1} and I am a ${profession} `;
-// }
-//console.log(showBio());
+//Function expression
 
-function modify(strings, ...values) {
-  console.log(strings, values);
-  let str = "";
-  strings.forEach((strings, i) => {
-    str += `${strings}<b>${values[i] || ""}</b>`;
-  });
-  return str;
-}
+const add = function(num1 = 2, num2) {
+  return num1 + num2;
+};
 
-const html = modify`
-  <h1>Header</h1>
-  <p>I am a paragraph</p>
-  <p>My name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} and I am ${age -
-  1} and I am a ${profession}</P>   
-`;
-
-document.body.innerHTML = html;
+console.log(add(undefined, 1));
