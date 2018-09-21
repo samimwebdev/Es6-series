02d5fc3b84b174f981c68e98893c2ea45511a5fe
samim <samimfazlu@gmail.com>
2018-09-22 00:00:18 +0600

    array and object desturucturing completed

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
-//const myProfile = ["samim", "lastName", 26, "webdeveloper"];
-//Rest and spread operator
+// //const myProfile = ["samim", "lastName", 26, "webdeveloper"];
+// //Rest and spread operator
 
-// myProfile.push("programmer");
+// // myProfile.push("programmer");
 
-//spread operator
-//const updatedProfile = [...myProfile, "Programmer"];
+// //spread operator
+// //const updatedProfile = [...myProfile, "Programmer"];
 
-// const groceries = ["wheat", "banana"];
-// const anotherGroceries = ["salt"];
-// const updatedGroceries = [...groceries, "milk", ...anotherGroceries];
-//console.log(updatedGroceries);
+// // const groceries = ["wheat", "banana"];
+// // const anotherGroceries = ["salt"];
+// // const updatedGroceries = [...groceries, "milk", ...anotherGroceries];
+// //console.log(updatedGroceries);
 
-// const firstName = "samim";
-// const updatedName = [...firstName];
-// console.log(updatedName);
+// // const firstName = "samim";
+// // const updatedName = [...firstName];
+// // console.log(updatedName);
 
-//Rest operator
+// //Rest operator
 
-function sayBio(firstName, age, ...values) {
-  //Rest operator
-  return {
-    firstName: firstName,
-    age: age,
-    rest: [...values, "Jobless"] //spread operator
-  };
-}
-console.log(sayBio("samim", 25, "webdeveloper", "Programmer"));
+// function sayBio(firstName, age, ...values) {//Rest operator
+//   return {
+//     firstName: firstName,
+//     age: age,
+//     rest: [...values, "Jobless"]//spread operator
+//   };
+// }
+// console.log(sayBio("samim", 25, "webdeveloper", "Programmer"));
+
+//Array destrucuring
+
+// const myProfile = ["samim", "fazlu", 26, "webdeveloper"];
+// const [, , age] = myProfile;
+// console.log(age);
+
+//Object destrucuring
+const post = {
+  id: 1,
+  title: "sample title",
+  body: "sample body",
+  allowComments: true,
+  share: {
+    facebook: true,
+    twitter: true,
+    linkedin: false
+  }
+};
+//const { title, allowComments } = post;
+
+const { facebook } = post.share;
+console.log(facebook);
+//console.log(title, allowComments);
