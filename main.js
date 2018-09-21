c5472b6bc72f69b84013df37a3719e1fb9316c20
samim <samimfazlu@gmail.com>
2018-09-21 23:17:15 +0600

    Small demostration project

--- main.js
+++ main.js
@@ -1,25 +1,104 @@
-const marks = [80, 92, 95, 78, 85];
+const comments = [
+  {
+    postId: 1,
+    id: 1,
+    name: "Ervin Howell",
+    email: "Eliseo@gardner.biz",
+    body:
+      "laudantium enim quasi est quidem magnam voluptate ipsam eos tempora quo necessitatibus dolor quam autem quasi reiciendis et nam sapiente accusantium"
+  },
+  {
+    postId: 1,
+    id: 2,
+    name: "Ervin Howell",
+    email: "Jayne_Kuhic@sydney.com",
+    body:
+      "est natus enim nihil est dolore omnis voluptatem numquam et omnis occaecati quod ullam at voluptatem error expedita pariatur nihil sint nostrum voluptatem reiciendis et"
+  },
+  {
+    postId: 1,
+    id: 3,
+    name: "odio adipisci rerum aut animi",
+    email: "Nikita@garfield.biz",
+    body:
+      "quia molestiae reprehenderit quasi aspernatur aut expedita occaecati aliquam eveniet laudantium omnis quibusdam delectus saepe quia accusamus maiores nam est cum et ducimus et vero voluptates excepturi deleniti ratione"
+  },
+  {
+    postId: 1,
+    id: 4,
+    name: "alias odio sit",
+    email: "Lew@alysha.tv",
+    body:
+      "non et atque occaecati deserunt quas accusantium unde odit nobis qui voluptatem quia voluptas consequuntur itaque dolor et qui rerum deleniti ut occaecati"
+  }
+];
 
-//Find Hedlper
-// const result = marks.find(mark => mark >= 90);
+const users = [
+  {
+    id: 3,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496"
+      }
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets"
+    }
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618"
+      }
+    }
+  },
+  {
+    id: 1,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618"
+      }
+    }
+  }
+];
 
-//FindIndex
-// const result = marks.findIndex(mark => mark >= 90);
+const id = 1;
+const findUser = users.find(user => user.id === id);
+// console.log(findUser);
 
-//Every Helper
-// const result = marks.every(mark => mark >= 90);
-
-//some helper
-// const result = marks.some(mark => mark >= 90);
-
-//Filter Helper
-//return array
-//const result = marks.filter(mark => mark >= 90);
-
-//Reduce helper
-//Add multiple number
-// const result = marks.reduce((sum, number) => {
-//   return (sum += number);
-// }, 0);
-
-console.log(result);
+const findComments = comments
+  .filter(comment => comment.name === findUser.name)
+  .map(commentBody => `<p>${commentBody.body}</p>`)
+  .join(", ")
+  .replace(/,/, "");
+document.body.innerHTML = findComments;
+//console.log(findComments);
