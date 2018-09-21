e5d28f4589af15a9ef7e9a13867a1a4a8a405a63
samim <samimfazlu@gmail.com>
2018-09-22 00:23:07 +0600

    practical use of destructuring

--- main.js
+++ main.js
@@ -1,51 +1,41 @@
-// //const myProfile = ["samim", "lastName", 26, "webdeveloper"];
-// //Rest and spread operator
+//working with function (destructuring in mind)
 
-// // myProfile.push("programmer");
+function bio({ lastName, firstName, age }) {
+  return `${firstName} ${lastName} is ${age} years old`;
+}
 
-// //spread operator
-// //const updatedProfile = [...myProfile, "Programmer"];
+console.log(
+  bio({
+    firstName: "samim",
+    lastName: "fazlu",
+    age: 25
+  })
+);
 
-// // const groceries = ["wheat", "banana"];
-// // const anotherGroceries = ["salt"];
-// // const updatedGroceries = [...groceries, "milk", ...anotherGroceries];
-// //console.log(updatedGroceries);
-
-// // const firstName = "samim";
-// // const updatedName = [...firstName];
-// // console.log(updatedName);
-
-// //Rest operator
-
-// function sayBio(firstName, age, ...values) {//Rest operator
-//   return {
-//     firstName: firstName,
-//     age: age,
-//     rest: [...values, "Jobless"]//spread operator
-//   };
-// }
-// console.log(sayBio("samim", 25, "webdeveloper", "Programmer"));
-
-//Array destrucuring
-
-// const myProfile = ["samim", "fazlu", 26, "webdeveloper"];
-// const [, , age] = myProfile;
-// console.log(age);
-
-//Object destrucuring
-const post = {
-  id: 1,
-  title: "sample title",
-  body: "sample body",
-  allowComments: true,
-  share: {
-    facebook: true,
-    twitter: true,
-    linkedin: false
+const comments = [
+  {
+    id: 10,
+    body: "first comment"
+  },
+  {
+    id: 2,
+    body: "second comment"
+  },
+  {
+    id: 4,
+    body: "dump comment"
+  },
+  {
+    id: 3,
+    body: "correct comment"
   }
-};
-//const { title, allowComments } = post;
+];
 
-const { facebook } = post.share;
-console.log(facebook);
-//console.log(title, allowComments);
+const badComment = comments.findIndex(comment => comment.id === 4);
+console.log(badComment);
+
+const choosenComment = [
+  ...comments.slice(0, badComment),
+  ...comments.slice(badComment + 1)
+];
+console.log(choosenComment);
