9b9f72314d37f9b2e8219cd066668830b0b40e34
samim <samimfazlu@gmail.com>
2018-09-21 20:01:33 +0600

    ForEach helper and map helper

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
-//Number addition
-//Function statement
-// function add(num1 = 3, num2) {
-//   return num1 + num2;
-// }
+const marks = [80, 95, 78, 85];
+// Iterate each number
+//return an array with ultimate result (new array)
 
-//Function expression
-
-//Arrow function
-// const add = (num1 = 2, num2) => {
-//   return num1 + num2;
-// };
-
-// console.log(add(undefined, 1));
-
-//If you return a single statement don't have to use {}
-//const add = (num1 = 2, num2) => num1 + num2;
-
-//if you want to use single paramete don't have to use ()
-//const add = num1 => num1 + 3;
-//console.log(add(1));
-
-//Where not to use arrow function
-const self = {
-  firstName: "samim",
-  age: 25,
-  showBio: function() {
-    console.log(this);
-    return `I am ${this.firstName} and I am ${this.age} `;
-  }
-};
-
-console.log(self.showBio());
+// const dbleMarks = marks.map(mark => mark * 2);
+// console.log(dbleMarks);
+//console.log(marks);
+let dbl = [];
+marks.forEach(mark => {
+  dbl.push(mark * 2);
+});
+console.log(dbl);
