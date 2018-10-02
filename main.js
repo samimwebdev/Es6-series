15d60cacd1def5ee9f844ae4e35af2576c849166
samim <samimfazlu@gmail.com>
2018-10-02 14:01:36 -0700

    Looping option with Javascript- Problem and solution

--- main.js
+++ main.js
@@ -1,43 +1,25 @@
-constructor way
-new keyword
-
-const programming = {
-  first: 'Javascript',
-  second: 'php',
-  third: 'python',
-  common:['Html', 'css', 'basic javascript'],
-  choosenLanguage: function(){
-    return `My first Priority-${this.first}.My second Priority-${this.second}`
-  }
-}
-console.log(programming.first);
-console.log(programming.second);
-console.log(programming.choosenLanguage());
+//Looping option
 
 
-//Es6 improved way of writing Object literal
-function bio({firstName, lastName, choosenLng}){
-  return{
-    firstName,
-    lastName,
-    common:['Html', 'css', 'basic javascript'],
-    choosenLanguage(){
-      return `My first NAme-${this.firstName}.My second Name-${this.lastName} and I prefer ${choosenLng}`
-    }
-  }
-}
+
+//Simple for loop
+  //Syntaz is complex for beginner
+  //break and continue works hurrah
+
+//forEach loop
+  //simple syntax
+  //break and continue don't work
+
+//for in loop
+  //Returning index number not actual value
+  //perfect for object property and value Iteration
+
+//solution- for of loop
+  //Break and continue works
+  //simple syntax- easy to work with
+  //But not working with object by default  --oops
 
 
-const myBio = bio({
-  firstName: 'samim',
-  lastName: 'Fazlu',
-  choosenLng: 'Javascript' 
-});
-
-console.log(myBio.firstName);
-
-console.log(myBio.lastName);
-console.log(myBio.choosenLanguage())
 
 
 
