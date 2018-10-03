31ca2d9b7955fd529bcd25dcc7814bc2f45a113d
samim <samimfazlu@gmail.com>
2018-10-02 19:08:29 -0700

    ES6 Symbol

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-//Looping option
-
-
-
-//Simple for loop
-  //Syntaz is complex for beginner
-  //break and continue works hurrah
-
-//forEach loop
-  //simple syntax
-  //break and continue don't work
-
-//for in loop
-  //Returning index number not actual value
-  //perfect for object property and value Iteration
-
-//solution- for of loop
-  //Break and continue works
-  //simple syntax- easy to work with
-  //But not working with object by default  --oops
+//string
+//Number
+//Boolean
+//complex type - Object function
 
 
 
 
+//Symbol (unique identifer, uniquely generated value)
 
+
+// const samimAge = Symbol(26);
+// const AnisAge = Symbol(26);
+
+// console.log(samimAge === AnisAge);
+
+
+
+const bio = {
+  [Symbol('samim')]: {firstName: 'samim', age:26},
+  [Symbol('samim')]: {firstName: 'samim', age:28}
+}
+
+const syms = Object.getOwnPropertySymbols(bio);
+
+//const data = syms.map(data => bio[data]);
+//console.log(data[1].age);
+
+for(const data of syms){
+  console.log(bio[data]);
+}
 
